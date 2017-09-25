15e6d5aa00ea774dea95c76cc288f21172848efb
XiaZhengXin <xzx@xiazhengxin.name>
2017-09-25 17:19:09 +0800

    initial commit.

--- workers/models.py
+++ workers/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class TestModel(models.Model):
+    username = models.CharField(max_length=20)
+    age = models.IntegerField(default=1)
--- workers/urls.py
+++ workers/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
     url(r'^test$',views.doTest,name='doTest'),
+    url(r'^$',views.index,name='index'),
 ]
