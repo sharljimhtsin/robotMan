74a7e71783015900fcc43dc7bcf20422dc2956b6
XiaZhengXin <xzx@xiazhengxin.name>
2017-09-25 18:09:44 +0800

    initial commit.

--- workers/models.py
+++ workers/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class TestModel(models.Model):
     username = models.CharField(max_length=20)
     age = models.IntegerField(default=1)
+    def __str__(self):
+        return self.username
--- workers/views.py
+++ workers/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from workers.models import TestModel
 
-def  index(request):
-    return HttpResponse("hello world")
 
-def  doTest(request):
+def index(request):
+    data = TestModel.objects.all()
+    print(data)
+    resp = "".join(m.__str__() for m in data)
+    return HttpResponse("hello world" + resp)
+
+
+def doTest(request):
     return HttpResponse('hahahah')
+
+
+def doPost(postData, hostName, subUrl):
+    import http.client, urllib.parse
+    params = urllib.parse.urlencode(postData)
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    conn = http.client.HTTPConnection(hostName)
+    conn.request("POST", subUrl, params, headers)
+    response = conn.getresponse()
+    print(response.status, response.reason)
+    data = response.read()
+    conn.close()
