5ab83b134450ed88940fcbfbf2e08b82762a3e8d
XiaZhengXin <xzx@xiazhengxin.name>
2017-09-26 13:47:15 +0800

    initial commit.

--- workers/admin.py
+++ workers/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import TestModel
+from .models import TestModel, Topic, Comment, FinishedWork, User
 
-admin.site.register(TestModel)
 # Register your models here.
+admin.site.register(TestModel)
+admin.site.register(Topic)
+admin.site.register(Comment)
+admin.site.register(FinishedWork)
+admin.site.register(User)
--- workers/models.py
+++ workers/models.py
@@ -1,7 +1,47 @@
 from django.db import models
 
+
 class TestModel(models.Model):
     username = models.CharField(max_length=20)
     age = models.IntegerField(default=1)
+
     def __str__(self):
         return self.username
+
+
+class Topic(models.Model):
+    id = models.IntegerField(primary_key=1)
+    title = models.TextField(max_length=50)
+    content = models.TextField(max_length=300)
+    imageUrls = models.TextField(max_length=200)
+    clubId = models.IntegerField(default=0)
+    lastTime = models.DateTimeField(auto_now_add=1)
+
+
+class Comment(models.Model):
+    id = models.IntegerField(primary_key=1)
+    postsId = models.ForeignKey(Topic, to_field='id')
+    content = models.TextField(max_length=300)
+    imageUrls = models.TextField(max_length=200)
+    lastTime = models.DateTimeField(auto_now_add=1)
+
+
+class FinishedWork(models.Model):
+    id = models.IntegerField(primary_key=1)
+    type = models.IntegerField(default=0)
+    contentId = models.IntegerField(default=0)
+    userId = models.IntegerField(default=0)
+    theTime = models.DateTimeField(auto_now_add=1)
+
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=1)
+    username = models.TextField(max_length=10)
+    password = models.TextField(max_length=20)
+    nickname = models.TextField(max_length=10)
+    avatar = models.TextField(max_length=20)
+    birthday = models.DateTimeField()
+    sex = models.IntegerField(default=1)
+    signature = models.TextField(max_length=50)
+    verifyCode = models.TextField(default='maimengmanhua', max_length=20)
+    lastTime = models.DateTimeField(auto_now_add=1)
